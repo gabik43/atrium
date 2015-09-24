3ce4b73700fd20e115a834b0880eeb7f085d83d3
Timofey <tims333@mail.ru>
2015-09-24 13:54:37 +0300

    Изменён класс ReportDWH (для записи отчётности о неправильно заполненных полях DWH)

--- AtriumProject/src/test/java/xml/Main.java
+++ AtriumProject/src/test/java/xml/Main.java
@@ -1,10 +1,15 @@
 package xml;
 
+import ru.asteros.atrium.Report.ReportDWH;
+
 /**
  * Created by A.Gabdrakhmanov on 10.04.2015.
  */
 public class Main {
 
     public static void main(String args[]) {
+
+        ReportDWH.addWarning(212, "12assd", "sdafsdfsdf", "sdfsdfsdfsdfsdf");
+
     }
 }
