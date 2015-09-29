4067dee7587cf83817b406511620b2630d4ba0ea
Gabik <gabik@sibnet.ru>
2015-09-29 10:16:37 +0300

    Для тестов

--- AtriumFX/src/atrium/fx/ru/Main.java
+++ AtriumFX/src/atrium/fx/ru/Main.java
@@ -1,13 +1,10 @@
 package atrium.fx.ru;
 
-import atrium.fx.ru.arhive.ZIPArchive;
 import atrium.fx.ru.core.ClientApplication;
 import atrium.fx.ru.core.ModalWindow;
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.stage.Stage;
 
-import java.io.File;
 import java.io.IOException;
 
 public class Main extends Application {
@@ -19,8 +16,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        //LogDB.setupConnection();
-        //LogDB.info("Start client application");
+        LogDB.setupConnection();
+        LogDB.info("Start client application");
         //AppConfiguration.readConfigFile();
 
         InternalInterface.modalWindow = new ModalWindow(stage);
