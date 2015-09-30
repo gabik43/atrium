f5fb75880550621b6dbcf13fb74e4baebc75f091
Gabik <gabik@sibnet.ru>
2015-09-30 10:59:31 +0300

    Тестовые методы

--- AtriumProject/src/test/java/xml/Main.java
+++ AtriumProject/src/test/java/xml/Main.java
@@ -1,17 +1,13 @@
 package xml;
 
-import ru.asteros.atrium.stringVerification.ClearedStringAndString;
-import ru.asteros.atrium.stringVerification.StringVerification;
-
 /**
- * Created by A.Gabdrakhmanov on 10.04.2015.
+ *
  */
 public class Main {
 
     public static void main(String args[]) {
-        String name = " ........42831312_7714072839_ВГТРК \\ГТРКСлавия_2015-08-31*.pdf. ";
-        String error = "";
-        ClearedStringAndString clearedStringAndString = StringVerification.getClearedStringAndStringWitchErrorSymbols(name);
-        int i = 0;
+        Integer a = 100;
+        Integer b = 100;
+        System.out.print(a == b);
     }
 }
